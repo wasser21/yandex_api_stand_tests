cdf65edc28549c953da75efb330cb74c0c8991b2
Юрий <urij@MBP-Urij.Dlink>
2023-11-11 14:09:12 +0200

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://f0cfeb45-c157-485f-bdcc-95e8be114424.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
